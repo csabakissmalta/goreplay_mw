1dc3cfce9bcc0ccd47c9b6702bd67233d87a28e6
Csaba Kiss <csaba.kiss@tipico.com>
2021-08-27 21:12:09 +0200

    stop writing orig responses #20

--- goreplay_mw.go
+++ goreplay_mw.go
@@ -89,24 +89,24 @@ func process(buf []byte) {
 			sessionIDs[reqID] = *new(old_to_new)
 			Debug(reqID)
 		} else {
-			return
+			ele := proto.Header(payload, []byte("Cookie"))
+			resp := get_session_id_from_cookie([]string{string(ele)})
+
+			for _, val := range sessionIDs {
+				if strings.TrimSpace(val.old) == strings.TrimSpace(resp) {
+					new_cookie := create_cookie_value_from_list(val.new)
+					payload = proto.SetHeader(payload, []byte("Cookie"), []byte(new_cookie))
+					buf = append(buf[:headerSize], payload...)
+					Debug("REQ", new_cookie, val.new)
+					os.Stdout.Write(encode(buf))
+					return
+				}
+			}
 		}
 
 		// for key, _ := range hs {
 		// if key == "Cookie" {
-		ele := proto.Header(payload, []byte("Cookie"))
-		resp := get_session_id_from_cookie([]string{string(ele)})
 
-		for _, val := range sessionIDs {
-			if strings.TrimSpace(val.old) == strings.TrimSpace(resp) {
-				new_cookie := create_cookie_value_from_list(val.new)
-				payload = proto.SetHeader(payload, []byte("Cookie"), []byte(new_cookie))
-				buf = append(buf[:headerSize], payload...)
-				Debug("REQ", new_cookie, val.new)
-				os.Stdout.Write(encode(buf))
-				return
-			}
-		}
 		// }
 		// }
 
