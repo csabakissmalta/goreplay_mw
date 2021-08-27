c9aa98765b6d8ed89ced79a6f190abff9ba068a3
Csaba Kiss <csaba.kiss@tipico.com>
2021-08-27 21:53:11 +0200

    stop writing orig responses #34

--- goreplay_mw.go
+++ goreplay_mw.go
@@ -93,17 +93,20 @@ func process(buf []byte) {
 			ele := proto.Header(payload, []byte("Cookie"))
 			resp := get_session_id_from_cookie([]string{string(ele)})
 
-			for _, val := range sessionIDs {
-				if strings.TrimSpace(val.old) == strings.TrimSpace(resp) {
-					new_cookie := create_cookie_value_from_list(val.new)
-					Debug("NJU KOOKI: ", new_cookie)
-					payload = proto.SetHeader(payload, []byte("Cookie"), []byte(new_cookie))
-					buf = append(buf[:headerSize], payload...)
-					// Debug("REQ", new_cookie, val.new)
-					os.Stdout.Write(encode(buf))
-					return
-				}
-			}
+			Debug("ele: ", ele)
+			Debug("resp: ", resp)
+
+			// for _, val := range sessionIDs {
+			// 	if strings.TrimSpace(val.old) == strings.TrimSpace(resp) {
+			// 		new_cookie := create_cookie_value_from_list(val.new)
+
+			// 		payload = proto.SetHeader(payload, []byte("Cookie"), []byte(new_cookie))
+			// 		buf = append(buf[:headerSize], payload...)
+			// 		// Debug("REQ", new_cookie, val.new)
+			// 		os.Stdout.Write(encode(buf))
+			// 		return
+			// 	}
+			// }
 		}
 		os.Stdout.Write(encode(buf))
 		Debug("ORIG")
